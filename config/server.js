2441aebe4037f0b4ba1872ac29e8c38acbbc7602
André Dargains <andre.dargains@gmail.com>
2020-10-27 23:55:39 +0000

    UPD: server production;

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', 'https://strapidargains.azurewebsites.net'),
   // host: env('HOST', '13.69.68.0'),
   port: env.int('PORT', 1337),
+  production: true,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ce7445a0d13c5690c39d05738a338d5d'),
